c6d395bbcdc16ffa91159b6c264de0652b8afd20
dazzes4545 <dazzes7619@gmail.com>
2026-04-08 14:31:25 +0300

    Added class Todo

--- bin/todo_app.dart
+++ bin/todo_app.dart
@@ -1,3 +1,18 @@
 void main(List<String> args) {
   print('Hello world');
+  String name = 'Артём';
+  String? name2 = null;
+  var count = 0;
+  var title = "hello";
+  //var x = 'dsf';
+  final id;
+  id = 5;
+  const app = "Todo";
+  print('$app $id ${count + 5}');
+
+  List<String> test = ['язык', 'уроки'];
+  Map<String, dynamic> data = {'key': 13};
+  Set<int> id2 = {1,2,3};
+
+  
 }
